9e52358695321e678090993555d1e0f98ac93ba9
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-02 13:13:21 +0530

    Added ability to analyze happy or sad mood.

--- src/main/java/MoodAnalyserMain.java
+++ src/main/java/MoodAnalyserMain.java
@@ -1,15 +1,25 @@
 
 /**
- * Purpose  - Mood Analyzer problem
+ * Purpose  -  ability to analyse and respond Happy or Sad Mood
  * @author  - Ashvini Kanojia
  * @version - 16.0
  * @since   - 01-04-2022
  */
 
+/**
+ * create a class name as MoodAnalyserMain
+ */
 public class MoodAnalyserMain {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to mood analyzer program");
+    /**
+     * create a constructor ,this is default constructor name as MoodAnalyserMain
+     */
+    public MoodAnalyserMain() {
     }
 
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
+}
