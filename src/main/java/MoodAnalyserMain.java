b5171a015f1a54acc8fec801eed09f3f1cd4af40
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-02 13:29:01 +0530

    Added to refactor and used constructor`.

--- src/main/java/MoodAnalyserMain.java
+++ src/main/java/MoodAnalyserMain.java
@@ -1,6 +1,6 @@
 
 /**
- * Purpose  -  I am in Any Mood ,message Should Return HAPPY
+ * Purpose  - Refactor the code to take the mood message in Constructor
  * @author  - Ashvini Kanojia
  * @version - 16.0
  * @since   - 01-04-2022
@@ -10,14 +10,18 @@
  * create a class name as MoodAnalyserMain
  */
 public class MoodAnalyserMain {
+    private String message;
     /**
      * create a constructor ,this is default constructor name as MoodAnalyserMain
      */
     public MoodAnalyserMain() {
     }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
+    public MoodAnalyserMain(String message) {
+        this.message=message;
+    }
+    public String analyseMood() {
+        if (this.message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
