a469407962077b538a9c61c1adb9dc5a559c00b8
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-02 13:09:12 +0530

    Added the Welcome message.

---
+++ src/main/java/MoodAnalyserMain.java
@@ -0,0 +1,15 @@
+
+/**
+ * Purpose  - Mood Analyzer problem
+ * @author  - Ashvini Kanojia
+ * @version - 16.0
+ * @since   - 01-04-2022
+ */
+
+public class MoodAnalyserMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to mood analyzer program");
+    }
+
+    }
